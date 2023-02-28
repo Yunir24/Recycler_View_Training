c4df977254d02b47630141cd3d5e2643d2a1b473
Yunir Dautov <dautovyunir93@gmail.com>
2023-02-28 11:51:51 +0500

    Creating project

---
+++ app/src/main/java/com/example/recyclerviewtraining/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.recyclerviewtraining
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.recyclerviewtraining.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = PlantAdapter()
+    private val imageList = listOf(
+        R.drawable.img10,
+        R.drawable.img11,
+        R.drawable.img12,
+        R.drawable.img13,
+        R.drawable.img14,
+        R.drawable.img15,
+        R.drawable.img16,
+        R.drawable.img17,
+        R.drawable.img18,
+        R.drawable.img19,
+        R.drawable.img20,
+        R.drawable.img21,
+        R.drawable.img22,
+        R.drawable.img24,
+        R.drawable.img3,
+        R.drawable.img4,
+        R.drawable.img5,
+        R.drawable.img6,
+        R.drawable.img7,
+        R.drawable.img8,
+        R.drawable.img9,
+        R.drawable.img2,
+        R.drawable.img23
+    )
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+
+    }
+
+    private fun init() {
+        binding.apply {
+            recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 2)
+
+            recyclerView.adapter = adapter
+            for (coun in imageList.indices){
+                adapter.addPlant(Plant(R.drawable.img15, "Пупсики $coun"))
+            }
+
+            button.setOnClickListener {
+//                if (count >= imageList.size)
+//                    count = 0
+//                val plant = Plant(imageList[count], "Пупсики $count")
+//                adapter.addPlant(plant)
+//                count++
+                var inten = Intent(this@MainActivity,MainActivity2::class.java)
+                startActivity(inten)
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewtraining/PlantAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.recyclerviewtraining
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewtraining.databinding.MyPhotoBinding
+
+
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
+    val plantList = mutableListOf<Plant>()
+
+
+
+    class PlantHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val binding = MyPhotoBinding.bind(item)
+        fun bind(plant: Plant) = with(binding) {
+            imageView.setImageResource(plant.imageId)
+            textView.text = "Пупсики"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.my_photo, parent, false)
+        return PlantHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlantHolder, position: Int) {
+        val plant = plantList[position]
+        holder.bind(plant)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return plantList.size
+    }
+
+    fun addPlant(plant: Plant){
+        plantList.add(plant)
+        notifyDataSetChanged()
+    }
+
+}
